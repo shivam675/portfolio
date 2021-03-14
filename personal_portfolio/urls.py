6787aa1a87cdfc921f31acb224e3868aca89d1a0
shivam675 <shivam31199@gmail.com>
2021-03-14 23:45:00 +0530

    added login signup and logout

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -23,6 +23,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('blog/', include('blog.urls')),
+    path('signupuser', views.signupuser, name='signupuser'),
+    path('loginuser', views.loginuser, name='loginuser'),
+    path('logout/', views.logoutuser, name='logoutuser'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login, logout, authenticate
 from .models import Project
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     projects = Project.objects.all()
     return render(request, 'portfolio/home.html', {'projects':projects})
+def signupuser(request):
+    if request.method == 'GET':
+        return render(request, 'portfolio/signupuser.html', {'form':UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('home')
+            except IntegrityError:
+                return render(request, 'portfolio/signupuser.html', {'form':UserCreationForm(), 'error':'That username has already been taken. Please choose a new username'})
+        else:
+            return render(request, 'portfolio/signupuser.html', {'form':UserCreationForm(), 'error':'Passwords did not match'})
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'portfolio/loginuser.html', {'form':AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'portfolio/loginuser.html', {'form':AuthenticationForm(), 'error':'Username and password did not match'})
+        else:
+            login(request, user)
+            return redirect('home')
+
+@login_required
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
