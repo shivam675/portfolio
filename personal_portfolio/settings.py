9c00da26655682fcf9c09f6c852569b0053446f7
shivam675 <shivam31199@gmail.com>
2021-04-16 12:05:02 +0530

    Admin css fault rectified

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -122,6 +122,6 @@ STATIC_URL = '/static/'
 # STATICFILES_DIRS = ( os.path.join('static'),
                     #  os.path.join('portfolio/static'),)
 # STATIC_ROOT = ''
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
